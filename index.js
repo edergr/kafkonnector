a1e9771490e4ee60e829455bfadf898987bc6cbb
edergr <eder.rodrigues@minu.co>
2023-12-04 15:43:36 -0300

    [KC-001] Adiciona primeiras rotas, database, config e logger.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const pkg = require('./package.json');
 const server = require('./lib/server');
+const { database, logger } = require('./lib/commons');
 
 process.title = pkg.name;
 
 const shutdown = async () => {
+  await database.close();
   await server.stop();
   process.exit(0);
 };
@@ -12,23 +14,24 @@ process.on('SIGTERM', shutdown)
   .on('SIGINT', shutdown)
   .on('SIGHUP', shutdown)
   .on('uncaughtException', (err) => {
-    console.log('uncaughtException caught the error: ', err);
+    logger.error('uncaughtException caught the error: ', err);
     throw err;
   })
   .on('unhandledRejection', (err, promise) => {
-    console.log(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
+    logger.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
     throw err;
   })
   .on('exit', (code) => {
-    console.log(`Node process exit with code: ${code}`);
+    logger.info(`Node process exit with code: ${code}`);
   });
 
 (async () => {
   try {
     await server.start();
+    await database.connect();
   } catch (err) {
-    console.log('Initialization failed', err);
+    logger.error('Initialization failed', err);
     throw err;
   }
-  console.log('Initialized successfully');
+  logger.info('Initialized successfully');
 })();
